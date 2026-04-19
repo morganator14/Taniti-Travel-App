42e455ef84f10bbbbb54230b81d1928897cf804a
Morgan Toronyi <morgant@hsppr.org>
2026-04-19 14:47:19 -0600

    lodging layout

--- D479Task/src/app/activities/activities.ts
+++ D479Task/src/app/activities/activities.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-activities',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './activities.html',
   styleUrl: './activities.css',
 })
--- D479Task/src/app/lodging/lodging.ts
+++ D479Task/src/app/lodging/lodging.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-lodging',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './lodging.html',
   styleUrl: './lodging.css',
 })
@@ -13,4 +13,18 @@ export class Lodging {
   goToPage(route:string) {
     this.router.navigate([route])
   }
+    @HostListener('window:scroll', [])
+  onScroll(): void {
+  const scrollY = window.scrollY;
+  const fadeStart = 0;
+  const fadeEnd = 200;
+
+  const opacity = 1 - Math.min(scrollY / (fadeEnd - fadeStart), 1);
+
+  const fixedTop = document.querySelector('.fixed-top') as HTMLElement;
+  if (fixedTop) {
+    fixedTop.style.opacity = String(opacity);
+    fixedTop.style.pointerEvents = opacity === 0 ? 'none' : 'auto';
+  }
+}
 }
